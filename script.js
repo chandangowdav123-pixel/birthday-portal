ea096d183a11a349347579b518983c7ec5601e88
chandangowdav123-pixel <chandangowdav123@gmail.com>
2026-03-01 17:15:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-function generateWish() {
-
-    const fixedName = "Chinmayi";   // 👈 Change name here anytime
-    const message = document.getElementById("wishMessage");
-
-    message.innerHTML = "🎉 Happy Birthday " + fixedName + "! 🎉";
-    launchConfetti();
-}
-
-function launchConfetti() {
-    const duration = 3 * 1000;
-    const end = Date.now() + duration;
-
-    (function frame() {
-        confetti({
-            particleCount: 5,
-            angle: 60,
-            spread: 55,
-            origin: { x: 0 }
-        });
-
-        confetti({
-            particleCount: 5,
-            angle: 120,
-            spread: 55,
-            origin: { x: 1 }
-        });
-
-        if (Date.now() < end) {
-            requestAnimationFrame(frame);
-        }
-    })();
+function goToBirthday() {
+    window.location.href = "birthday.html";
 }
